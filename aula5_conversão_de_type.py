20fc6794c8fe2517cb4b183a3f12467172c26cb4
Milena Yamamoto <milymanamy3@gmail.com>
2023-07-04 05:18:11 -0300

    .

--- aula5_conversão_de_type.py
+++ aula5_conversão_de_type.py
@@ -1,7 +1,7 @@
 # conversão de tipos, coerção
 # type convertion, typecasting, coercion
 # é o ato de converter um tipo em outro
-#  tipos imutáveis e primitivos:
+# tipos imutáveis e primitivos:
 # str, int, float, bool
 print(int('1'), type(int('1')))
 print(type(float('1') + 1))
--- aula6_variaveis.py
+++ aula6_variaveis.py
@@ -7,7 +7,7 @@
 
 # nome_completo = 'Luiz Otávio Miranda'
 # soma_dois_mais_dois = 2 + 2
-# int_um = bool('1')
+# int_um = int('1')
 # print(int_um, type(int_um))
 # print(nome_completo, soma_dois_mais_dois)
 
