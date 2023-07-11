85b2aebe1f86eb24cc85482e351c4ed9a193c784
Milena Yamamoto <milymanamy3@gmail.com>
2023-07-11 06:26:01 -0300

    .

--- aula15.input.py
+++ aula15.input.py
@@ -11,4 +11,4 @@ int_numero_2 = int(numero_2)
 
 print(f'A soma dos números é: {int_numero_1 + int_numero_2}')
 #se nos input o usuário colocar números, a soma do f-strings dará certo.
-#se o usuário colocar letras, dará erro, pois f-strings está usando variáveis do tipo inteiro.
+#se o usuário colocar letras, dará erro, pois não tem como transformar uma letra em inteiro.
--- aula29.try_e_except.py
+++ aula29.try_e_except.py
@@ -3,19 +3,22 @@ Introdução ao try/except
 try -> tentar executar o código
 except -> ocorreu algum erro ao tentar executar
 """
+#duas maneiras do usuário conseguir apenas digitar NÚMEROS sem da erro!
+
 numero_str = input(
     'Vou dobrar o número que vc digitar: '
 )
-
+#maneira 1 - usando try e except
 try:
     numero_float = float(numero_str)
-    print('FLOAT:', numero_float)
     print(f'O dobro de {numero_str} é {numero_float * 2:.2f}')
 except:
     print('Isso não é um número')
 
-# if numero_str.isdigit():
-#     numero_float = float(numero_str)
-#     print(f'O dobro de {numero_str} é {numero_float * 2:.2f}')
-# else:
-#     print('Isso não é um número')
+#maneira 2 - usando if e else com a função isdigit()
+
+if numero_str.isdigit():
+    numero_float = float(numero_str)
+    print(f'O dobro de {numero_str} é {numero_float * 2:.2f}')
+else:
+    print('Isso não é um número')
