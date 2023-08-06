752b12d80093b6f46702adb1c85db53e06b386c3
Milena Yamamoto <milymanamy3@gmail.com>
2023-08-06 10:08:13 -0300

    .

--- aula44.calculadora_com_while.py
+++ aula44.calculadora_com_while.py
@@ -1,8 +1,7 @@
 """ Calculadora com while """
 while True:
     print('nummmmm')
-    #########
-    sair = input('Quer sair? [s]im: ').lower().startswith('s')
+    sair = input('Quer sair? [s]im: ').lower().startswith('s') ##startswith é boolean
 
     if sair is True:
         break
