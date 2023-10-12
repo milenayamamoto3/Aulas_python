f2b2d9f6ddeaa5ba36aa83f4a23a959ebfbcfcc8
Milena Yamamoto <milymanamy3@gmail.com>
2023-10-12 08:24:21 -0300

    .

--- aula123modularizacao.py
+++ aula123modularizacao.py
@@ -20,3 +20,17 @@ import aula123modularizacao_m #módulo onde seu local por padrão já está adc
 
 print('Este módulo se chama', __name__) #executado primeiro, módulo main
 print(*sys.path, sep='\n') #exibe os locais onde os módulos podem ser importados
+                            #o asterisco desempacota a lista, pois o path é LISTA
+
+print(aula123modularizacao_m.variavel_1)  #dessa maneira, chamamos a variável
+from aula123modularizacao_m import variavel_1  #importando a variável
+print(variavel_1)  #printando a variável que já está importada
+
+import importlib 
+for i in range(10):
+    importlib.reload(aula123modularizacao_m) #recarregar o módulo
+    print(i)
+
+from aula123modularizacao_m import *
+print(variavel) #consegue puxar
+# print(variavel_2) -> não consegue puxar, pois não está no __all__
