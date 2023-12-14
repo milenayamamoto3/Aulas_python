c4a586bac4aeb303dbf3c71e02820af141ba709d
Milena Yamamoto <milymanamy3@gmail.com>
2023-12-14 09:29:43 -0300

    .

--- aulas/aula146criando_arquivo.py
+++ aulas/aula146criando_arquivo.py
@@ -1,4 +1,5 @@
 # Criando arquivos com Python + Context Manager with
+# with open (context manager) e métodos úteis do TextIOWrapper(type(arquivo))
 # Usamos a função open para abrir
 # um arquivo em Python (ele pode ou não existir)
 # Modos:
@@ -15,9 +16,8 @@
 # Vamos falar mais sobre o módulo os, mas:
 # os.remove ou unlink - apaga o arquivo
 # os.rename - troca o nome ou move o arquivo
-# Vamos falar mais sobre o módulo json, mas:
-# json.dump = Gera um arquivo json
-# json.load
+
+# Leia também: https://www.otaviomiranda.com.br/2020/normalizacao-unicode-em-python/
 
 caminho_arquivo = 'C:\\Users\\acerc\\OneDrive\\Área de Trabalho\\projetos_python\\aulas'
 caminho_arquivo += 'aula146.txt'
@@ -26,6 +26,36 @@ caminho_arquivo += 'aula146.txt'
 # #
 # arquivo.close()
 
-with open(caminho_arquivo, 'w') as arquivo:
-    print('Olá mundo')
-    print('Arquivo vai ser fechado')
+with open(caminho_arquivo, 'w+', encoding='utf8') as arquivo: #abre um novo arquivo para escrever e +
+    print(type(arquivo)) #tipo de arquivo
+    arquivo.write('Atenção\n') #cedilha e tio precisa "encoding='utf8'"
+    arquivo.write('Linha 1\n') #escrevendo uma linha
+    arquivo.write('Linha 2\n')
+    arquivo.writelines(        #escrevendo várias linhas
+        ('Linha 3\n', 'Linha 4\n') 
+    )
+    arquivo.seek(0, 0) #volta o cursor para cima
+    print(arquivo.read()) #lendo o arquivo todo
+    print('Lendo')     #esse print só sai aqui
+    arquivo.seek(0, 0)
+    print(arquivo.readline(), end='') # ler uma linha apenas, igual "next"(interator), 
+    print(arquivo.readline().strip()) # end e strip para não pular pra outra linha
+    print(arquivo.readline().strip()) # excluindo o "\n" que o cursor faz automat.
+
+    print('READLINES')
+    arquivo.seek(0, 0)
+    for linha in arquivo.readlines():#todas as linhas
+        print(linha.strip())
+
+print('#' * 10) #print fora do with
+
+with open(caminho_arquivo, 'r') as arquivo: # abre o arquivo para ler
+    print(arquivo.read())
+
+# os dois métodos excluem o arquivo
+# os.remove(caminho_arquivo)
+# os.unlink(caminho_arquivo)
+
+# método de mover e renomear o arquivo
+# os.rename(caminho_arquivo, "aula1462.txt") -> primeiro parâmetro é o caminho
+#e o segundo o novo nome.
---
+++ aulas/aula147json.py
@@ -0,0 +1,33 @@
+# Vamos falar mais sobre o módulo json, mas:
+# json.dump = Gera um arquivo json
+# json.load = Carrega o json para o python
+import json
+
+pessoa = {
+    'nome': 'Luiz Otávio 2',
+    'sobrenome': 'Miranda',
+    'enderecos': [
+        {'rua': 'R1', 'numero': 32},
+        {'rua': 'R2', 'numero': 55},
+    ],
+    'altura': 1.8,
+    'numeros_preferidos': (2, 4, 6, 8, 10),
+    'dev': True,
+    'nada': None,
+}
+
+caminho_arquivo1 = 'C:\\Users\\acerc\\OneDrive\\Área de Trabalho\\projetos_python\\aulas'
+caminho_arquivo1 += 'aula147.json'
+
+with open(caminho_arquivo1, 'w', encoding='utf8') as arquivo1:
+    json.dump(pessoa,              #método escreve dados em formato JSON
+              arquivo1,            #(JavaScript Object Notation) para arquivo json
+              ensure_ascii=False,  #para os str se manter 
+              indent=2,            #para a estrutura se manter no arquivo criado
+    ) 
+                                
+with open(caminho_arquivo1, 'r', encoding='utf8') as arquivo1:
+    pessoa = json.load(arquivo1)
+    # print(pessoa) #a tupla que era antes virou lista! Cuidado!
+    # print(type(pessoa)) #dict
+    print(pessoa['nome']) #retorna o valor da chave "nome"
