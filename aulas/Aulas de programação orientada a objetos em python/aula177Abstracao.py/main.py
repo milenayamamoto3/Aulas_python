5a3e2150cb60db21aa1c4a14672b45eef653e305
Milena Yamamoto <milymanamy3@gmail.com>
2024-03-10 06:00:44 -0300

    .

--- aulas/Aulas de programação orientada a objetos em python/aula177Abstracao.py/main.py
+++ aulas/Aulas de programação orientada a objetos em python/aula177Abstracao.py/main.py
@@ -1,4 +1,4 @@
-from log import LogPrintMixin, LogFileMixin
+# from log import LogPrintMixin, LogFileMixin
 #from (pasta) import (classe)
 
 # lp = LogPrintMixin()
@@ -8,3 +8,12 @@ from log import LogPrintMixin, LogFileMixin
 # lf = LogFileMixin()
 # lf.log_error('qualquer coisa')
 # lf.log_sucess('Que legal!')
+
+from eletronico import Smartphone
+
+galaxy_s = Smartphone('Galaxy S')
+iphone = Smartphone('Iphone')
+
+galaxy_s.ligar()
+iphone.desligar()
+
