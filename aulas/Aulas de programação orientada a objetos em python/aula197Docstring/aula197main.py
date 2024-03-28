85fce896a757f7cfbc3b079a1076a48a1c5a7155
Milena Yamamoto <milymanamy3@gmail.com>
2024-03-28 07:25:31 -0300

    .

---
+++ aulas/Aulas de programação orientada a objetos em python/aula197Docstring/aula197main.py
@@ -0,0 +1,11 @@
+# import uma_linha
+# import varias_linhas
+# import documentando_em_funcoes
+import documentando_em_classes
+
+# print(dir(uma_linha)) #dir retorna lista de strgs desse módulo
+# print(uma_linha.__doc__) #retorna "None" pois não tem um doc criado nesse módulo
+# print(uma_linha.__file__) #retorna o caminho desse módulo
+# print(uma_linha.__name__) #retorna o nome desse módulo
+help(documentando_em_classes) #retorna o que tem dentro desse módulo
+#Dê um 'q' no terminal caso fique preso após executar a função 'help'
---
+++ aulas/Aulas de programação orientada a objetos em python/aula197Docstring/uma_linha.py
@@ -0,0 +1,6 @@
+"""Aqui você escrever um título""" # Doscstring de uma linha (cabeçalho)
+
+
+variavel = 'valor'
+def funcao():
+    return 1
